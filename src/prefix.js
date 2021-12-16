104659dc25fdd18e0bcea30f42f78551821d6344
Jakub Jankiewicz <jcubic@onet.pl>
2021-12-16 16:40:31 +0100

    add animation methods to Web Adapter

--- src/prefix.js
+++ src/prefix.js
@@ -36,6 +36,15 @@ class WebAdapter {
     echo(string) {
         this._term.echo(string);
     }
+    echo_animate(string, delay) {
+        this._term.echo(string, { typing: true, delay: 200 });
+    }
+    prompt_animate(string, delay) {
+        this._term.set_prompt(string, { typing: true, delay: 200 });
+    }
+    input_animate(string, delay) {
+        this._term.typing('enter', delay, string);
+    }
     get(url) {
         return fetch(url).then(res => res.text());
     }

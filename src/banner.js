d862a2c4c4aaa655fc088679a84bfbc52e89d1a4
Jakub Jankiewicz <jcubic@onet.pl>
2021-12-19 00:46:20 +0100

    add copyright banner file

---
+++ src/banner.js
@@ -0,0 +1,12 @@
+/**   ______      _
+ *   / ____/___ _(_)___ ___  ____ _____
+ *  / / __/ __ `/ / __ `__ \/ __ `/ __ \
+ * / /_/ / /_/ / / / / / / / /_/ / / / /
+ * \____/\__,_/_/_/ /_/ /_/\__,_/_/ /_/
+ *
+ * Storytelling Text Based Game Engine {{VER}}
+ * Copyright (C) 2021 Jakub T. Jankiewicz <https://jcubic.pl/me>
+ *
+ * Released under GNU GPL v3 or later
+ * Buid time: {{DATE}}
+ */

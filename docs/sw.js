e59ea24080f7027a6f9814028928b2a65f5c177a
Jakub Jankiewicz <jcubic@onet.pl>
2022-09-01 20:20:39 +0200

    add themes to playground

--- docs/sw.js
+++ docs/sw.js
@@ -61,6 +61,7 @@ function getStatic(url) {
                 }
                 resolve(textResponse(value, { type: getMime(extension) }));
             }).catch(error => {
+                debugger;
                 resolve(error500(error));
             });
         }
